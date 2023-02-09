c16f7c3317e2dd5183a98db0243e16444eaccc8f
sarwar-asik <shasik2021@gmail.com>
2023-02-09 22:29:54 +0600

    filtering

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,36 +1,64 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
+import { toggleBrand, toggleStock } from "../../redux/actions/filteraction";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const filters = useSelector(state=>state.filter.filters);
+  // console.log(filters);
+  const {brands,stock} =filters;
+  console.log(brands,stock);
+
 
   useEffect(() => {
     fetch("http://localhost:5000/products")
       .then((res) => res.json())
       .then((data) => setProducts(data.data));
   }, []);
+  const dispatch = useDispatch();
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
+  let content ;
+
+  if(products.length){
+   content = products?.map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ))
+  }
+  if(products.length && stock){
+    content = products
+    ?.filter(product =>product.status === true )?.map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ))
+  }
+
+
   return (
-    <div className='max-w-7xl gap-14 mx-auto my-10'>
-      <div className='mb-10 flex justify-end gap-5'>
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
         <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+          onClick={() => dispatch(toggleStock())}
+          className={`border px-3 py-2 rounded-full font-semibold ${stock?activeClass: null} `}
         >
           In Stock
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button
+          onClick={() => dispatch(toggleBrand("amd"))}
+          className={`border px-3 py-2 rounded-full font-semibold ${brands.includes("amd")? activeClass :null}`}
+        >
           AMD
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button
+          onClick={() => dispatch(toggleBrand("intel"))}
+          className={`border px-3 py-2 rounded-full font-semibold  ${brands.includes("intel")? activeClass :null}`}
+        >
           Intel
         </button>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
-        {products.map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
+        {content}
       </div>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import productReducer from "./reducers/productReducer";
+import rootReducer from "./reducers/rooteReducer";
 
-const store = createStore(productReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
